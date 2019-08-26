de2826026cff06070cfcd4e5e886d3cbb4738716
Andre <itstheandre@gmail.com>
2019-08-26 18:53:21 +0200

    Larger Arrays in different file. Code easier to Read, for sure)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const game = new Game();
 
-const player1 = new Player(9, 6);
+/* 
 const guard1 = new Guard(7, 3, "up");
 const guard2 = new Guard(7, 8, "up");
 const guard3 = new Guard(4, 9, "down");
@@ -8,7 +8,8 @@ const guard4 = new Guard(3, 4, "left");
 const guard5 = new Guard(1, 0, "down");
 const guard6 = new Guard(1, 3, "left");
 const guard7 = new Guard(2, 6, "up");
-const guard8 = new Guard(1, 9, "down");
+const guard8 = new Guard(1, 9, "down"); */
+
 //
 /* const player2 = new Player(9, 9);
 const guard9 = new Guard(6, 9, "up");
@@ -29,20 +30,9 @@ function setup() {
 }
 
 function draw() {
-  // if (game.level === 0) {
   game.draw();
-  player1.draw();
-  guard1.draw();
-  guard2.draw();
-  guard3.draw();
-  guard4.draw();
-  guard5.draw();
-  guard6.draw();
-  guard7.draw();
-  guard8.draw();
-  /* }
+  /* 
   if (game.level === 1) {
-    game.draw();
     player2.draw();
     guard9.draw();
     guard10.draw();
