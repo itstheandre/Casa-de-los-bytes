af994979e371a6762aec9e58c551fc7fe3c3fcbc
Andre <itstheandre@gmail.com>
2019-08-29 17:50:51 +0200

    Changed order of dom.js

--- js/dom.js
+++ js/dom.js
@@ -2,5 +2,5 @@ const button = document.querySelector(".button");
 button.onclick = function() {
   document.querySelector(".start").style.display = "none";
   startGame = true;
-  setup();
+  // setup();
 };
